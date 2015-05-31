f594fdd4a78994b9d952ef98958c0e72ffb127fc
janmilosh <janmilosh@gmail.com>
2015-05-31 10:54:10 -0400

    Adds comment

--- scripts/save_form_numbers_from_csv.py
+++ scripts/save_form_numbers_from_csv.py
@@ -20,20 +20,23 @@ class FormNumber:
                 forms = Form.objects.filter(canonical_url=row['URL'])
                 try:
                     form = forms[0]
-                    form.form_numbers = row['FORM #']
-                    print form.form_numbers, form.canonical_url
-                    print
+                    '''uncomment 3 lines below for adding form
+                    numbers to database
+                    '''
+                    # form.form_numbers = row['FORM #']
+                    # print form.form_numbers, form.canonical_url
+                    # print
 
                     '''In this section, add string that corresponds to
                     the comment in the FORM CHANGED? field for a form to
                     eliminate. Add additional strings as needed
                     '''
 
-                    if 'not a prescription' in row['FORM CHANGED?'].lower():
+                    if 'not a pa' in row['FORM CHANGED?'].lower():
                         form.ignore = True
                         print form.canonical_url, '****************', row['FORM CHANGED?'], '***************'
 
-                    form.save()
+                    # form.save()
                 except:
                     pass
 
