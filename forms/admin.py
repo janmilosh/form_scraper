6862dfd21efc059ac4c1fecde5961e2826bdf0ce
janmilosh <janmilosh@gmail.com>
2015-07-06 20:49:56 -0400

    Adds to admin search and adds admin link to menu

--- forms/admin.py
+++ forms/admin.py
@@ -5,6 +5,6 @@ from forms.models import Form
 
 class FormAdmin(admin.ModelAdmin):
     list_display = ('file_name', 'status_code', 'form_numbers', 'last_run_index', 'ignore',)
-    search_fields = ['file_name', 'canonical_url']
+    search_fields = ['file_name', 'canonical_url', 'form_numbers']
 
 admin.site.register(Form, FormAdmin)
